366c28096cadeb346c9bb6fc8f1aa95272de342d
GlebKurt <Q-he@twutown>
2024-11-03 10:49:32 +0100

    Homework done

--- homeworks/7_11_24.py
+++ homeworks/7_11_24.py
@@ -1,17 +1,27 @@
-print("Введіть координати для")
-y = int(input("Y: "))
-x = int(input("X: "))
+#1
+# print("Введіть координати для")
+# y = int(input("Y: "))
+# x = int(input("X: "))
 
-def coordinates():
-    if y < 0 and x < 0:
-        return "1 чверть"
-    elif y > 0 and x < 0:
-        return "2 чверть"
-    elif y > 0 and x > 0:
-        return "3 чверть"
-    elif y < 0 and x > 0:
-        return "4 чверть"
-    else:
-        "Точка знаходиться на осі"
+# def coordinates():
+#     if y < 0 and x < 0:
+#         return "1 чверть"
+#     elif y > 0 and x < 0:
+#         return "2 чверть"
+#     elif y > 0 and x > 0:
+#         return "3 чверть"
+#     elif y < 0 and x > 0:
+#         return "4 чверть"
+#     else:
+#         "Точка знаходиться на осі"
 
-print(coordinates())
+# print(coordinates())
+
+#2
+# surname = input("Введіть ваше прізвище: ")
+# score = int(input("Введіть вашу кількість балів: "))
+
+# if score >= 80:
+#     print(f"{surname} здав іспит.")
+# else:
+#     print(f"{surname} не здав іспит.")
