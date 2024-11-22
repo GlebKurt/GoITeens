92829cc6cf103aaec7b1f22dbc69f4e74f2c499b
GlebKurt <Q-he@twutown>
2024-11-22 15:03:40 +0100

    Homework added

---
+++ homeworks/23_11_24.py
@@ -0,0 +1,23 @@
+# 3
+# east = ['Northern America', 'South America', 'Antarctica']
+# west = ['Eurasia', 'Africa', 'Australia']
+
+# for materik in west:
+#     east.append(materik)
+# east.sort()
+# print(east)
+
+# 4
+# data = []
+
+# data1 = ['Андрій', '16', 'Ukraine']
+# data2 = ['Sasha', '13', 'Ukraine']
+# data3 = ['Андрій', '22', 'Ukraine']
+
+# data.extend(data1)
+# data.extend(data2)
+# data.extend(data3)
+
+# andriys = data.count('Андрій')
+
+# print(f"В цьому списку {andriys} Андріїв!")
